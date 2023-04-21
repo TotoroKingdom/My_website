6d97788e5ba855f972586eab1a0a7d94ed80e284
KallkaGo <765830637@qq.com>
2023-04-21 23:23:53 +0800

    style:调整介绍页面卡片布局

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -56,7 +56,7 @@ const About = () => {
         real-world problems.Also I am a fan of games from MMORPG to FPS, I wish I could be your friend!
 
       </motion.p>
-      <div className='mt-20 flex flex-wrap gap-10'>
+      <div className='mt-20 flex flex-wrap gap-10 justify-center '>
         {services.map((service, index) => {
           return (<ServiceCard key={service.title} index={index} {...service} />)
         })}
