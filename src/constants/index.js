08359473791f084f7195698bd138c1406d736349
KallkaGo <765830637@qq.com>
2023-05-31 02:12:38 +0800

    fix:修复Target页面显示错误的bug

--- src/constants/index.js
+++ src/constants/index.js
@@ -128,14 +128,48 @@ const experiences = [
   },
 ];
 
-const testimonials = [
+const targets = [
   {
     target:'I want to read and learn GLSL Shader Language book to improve myself',
-    progress:'15%'
+    name:'target-1'
   },
   {
-    target:'fitness every day'
+    target:'fitness every day',
+    name:'target-2'
+  },
+  {
+    target:"Develop projects with partners",
+    name:'target-3'
   }
+  // {
+  //   testimonial:
+  //     "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+  //   name: "Sara Lee",
+  //   designation: "CFO",
+  //   company: "Acme Co",
+  //   image: "https://randomuser.me/api/portraits/women/4.jpg",
+  // },
+  // {
+  //   testimonial:
+  //     "I've never met a web developer who truly cares about their clients' success like Rick does.",
+  //   name: "Chris Brown",
+  //   designation: "COO",
+  //   company: "DEF Corp",
+  //   image: "https://randomuser.me/api/portraits/men/5.jpg",
+  // },
+  // {
+  //   testimonial:
+  //     "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+  //   name: "Lisa Wang",
+  //   designation: "CTO",
+  //   company: "456 Enterprises",
+  //   image: "https://randomuser.me/api/portraits/women/6.jpg",
+  // },
+];
+
+
+const testimonials = [
+ 
   // {
   //   testimonial:
   //     "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
@@ -245,4 +279,4 @@ const projects = [
   }
 ];
 
-export { services, technologies, experiences, testimonials, projects };
+export { services, technologies, experiences, testimonials, projects,targets };
