ef8440ec9ad3f8308fa98387483d75c9584baa51
KallkaGo <765830637@qq.com>
2023-11-25 18:24:59 +0800

    feat: add customized information to the console

--- src/main.jsx
+++ src/main.jsx
@@ -8,3 +8,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </React.StrictMode>,
 )
+
+console.info('%cNice to meet you', "color: orange;background: ivory;font-size:26px;border: 2px solid black;padding:10px;text-shadow: 1px 1px grey;border-radius:11px;")
